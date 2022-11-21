f428929192b750160b2ae2a0b5fecf2bb9c8e2c9
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-11-21 14:40:54 +0200

    Update gitapi.js

--- gitapi.js
+++ gitapi.js
@@ -1,8 +1,8 @@
 
 let git = {
   
-  // get repos by language
-  'getRepos': async (language, pageNum = 1) => {
+  // search repos by language
+  'searchRepos': async (language, pageNum = 1) => {
     
     let query = 'https://api.github.com';
     
@@ -10,64 +10,44 @@ let git = {
              '?q=language:' + language + '&page=' + pageNum + '&sort=stars&order=desc&per_page=100';
    
     // get the query
-    const resp = await axios.get(query);
+    const resp = await axios.get(query, gitToken);
     
     return resp;
   
   },
-  
-  // get public file content
-  'getPublicFile': async (treeLoc, fileName) => {
-    
-    // map tree location
-    let query = 'https://raw.githubusercontent.com';
-    const [user, repo, contents] = treeLoc;
 
-    // get repository branch
-    let [repoName, branch] = repo.split(':');
-  
-    query += '/' + user + '/' + repoName +
-             '/' + branch +
-             '/' + contents + '/' + fileName;
-  
-    // get the query
-    const resp = await axios.get(query, '', true);
-    
-    return resp;
-        
-  },
-
-  // get items in tree
-  'getItems': async (treeLoc) => {
+  // search repository for files by language
+  'searchFiles': async (language, repoName, pageNum = 1) => {
 
     // map tree location
     let query = 'https://api.github.com';
-    const [user, repo, contents] = treeLoc;
-
-    // if navigating in repository
-    if (repo != '') {
-
-      // get repository branch
-      let [repoName, branch] = repo.split(':');
-
-      if (branch) branch = '?ref='+ branch;
-      else branch = '';
-
-      query += '/repos/' + user + '/' + repoName +
-               '/contents' + contents +
-               branch;
-
-    } else { // else, show all repositories
-
-      query += '/user/repos?visibility=all&sort=updated&per_page=100&page=1';
-
-    }
+    
+    query += '/search/code?q=language:' + language +
+             '+repo:' + repoName + '&page=' + pageNum +
+             '&sort=indexed&per_page=100';
 
     // get the query
-    const resp = await axios.get(query);
+    const resp = await axios.get(query, gitToken);
 
     return resp;
 
+  },
+  
+  // get public file content
+  'getFileContent': async (file) => {
+    
+    const repoName = file.repository.full_name;
+    const branch = file.url.split('?ref=')[1];
+    
+    let query = 'https://raw.githubusercontent.com';
+    
+    query += '/' + repoName + '/' + branch + '/' + file.path;
+  
+    // get the query
+    const resp = await axios.get(query, gitToken, true);
+    
+    return resp;
+        
   }
 
 };
