cb7df1dba6e36d02f48919c947b48669da11a0c1
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-11-21 14:08:44 +0200

    Update rankings.js

--- rankings.js
+++ rankings.js
@@ -1,2 +1,48 @@
 
-
+let axios = {
+  'get': (url, token, noParse) => {
+    return new Promise((resolve, reject) => {
+      try {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && String(this.status).startsWith('2')) {
+            try {
+              if (!noParse) {
+                resolve(JSON.parse(this.responseText));
+              } else {
+                resolve(this.responseText);
+              }
+            } catch (e) {
+              resolve();
+            }
+          } else if (this.responseText) {
+            try {
+              if (!noParse) {
+                resolve(JSON.parse(this.responseText));
+              } else {
+                resolve(this.responseText);
+              }
+            } catch (e) {}
+          }
+        };
+        xmlhttp.onerror = function() {
+          if (this.responseText) {
+            try {
+              if (!noParse) {
+                resolve(JSON.parse(this.responseText));
+              } else {
+                resolve(this.responseText);
+              }
+            } catch (e) {}
+          }
+        };
+
+        xmlhttp.open('GET', url, true);
+        xmlhttp.send();
+      } catch (e) {
+        reject(e)
+      }
+    });
+  }
+};
+
